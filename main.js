9263296ee53e2e7081d8e53d9c854103b722324e
dingkid <57839556+dingkid@users.noreply.github.com>
2026-03-15 22:10:13 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,28 +1,37 @@
 class MyHeader extends HTMLElement {
-    connectedCallback(){
+    connectedCallback() {
         this.innerHTML = `
         <div class="header-container p-3">
             <div class="col-5">
                 <h2 class="header-text">Jessica Ding: Portfolio</h2>
             </div>
-            <nav class="navbar mr-3">
-                <ul>
-                    <li><a href="index.html">Home</a></li>
-                    <li><a href="engineering-projects.html">Projects</a></li>
-                    <li><a href="gallery.html" title="Coming soon">Gallery</a></li>
-                    <li><a href="#" title="Coming soon">Music</a></li>
-                </ul>
-            </nav>
+            
         </div>
-        
         `
     }
 }
 
 customElements.define('my-header', MyHeader)
 
+class MyNavbar extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+          <nav class="">
+                <ul class="py-3">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="engineering-projects.html">Projects</a></li>
+                    <li><a href="gallery.html" title="Sorry, there's only one thing here.">Gallery</a></li>
+                    <li><a href="#" title="Coming soon">Music</a></li>
+                </ul>
+            </nav>
+        `
+    }
+}
+
+customElements.define('my-navbar', MyNavbar)
+
 class MyTitle extends HTMLElement {
-    connectedCallback(){
+    connectedCallback() {
         this.innerHTML = `
           
         `
@@ -32,7 +41,7 @@ class MyTitle extends HTMLElement {
 customElements.define('my-title', MyTitle)
 
 class MyBody extends HTMLElement {
-    connectedCallback(){
+    connectedCallback() {
         this.innerHTML = `
         
         `
@@ -42,11 +51,12 @@ class MyBody extends HTMLElement {
 customElements.define('my-body', MyBody)
 
 class MyFooter extends HTMLElement {
-    connectedCallback(){
+    connectedCallback() {
         this.innerHTML = `
-        <p class="footer-text py-3">
-        Last edited February 2026 by Jessica Ding.<br>
-         Email: <a href=mailto:"ding.je@northeastern.edu">ding.je@northeastern.edu</a> | <a href="https://www.linkedin.com/in/jlding" target="blank">LinkedIn</a>
+        <p class="py-3">
+        Last edited March 2026 by Jessica Ding.<br>
+        <b>CONTACT: </b>
+        <span>&#9993;</span><a href=mailto:"ding.je@northeastern.edu">ding.je@northeastern.edu</a> | <a href="https://www.linkedin.com/in/jlding" target="blank">LinkedIn</a>
         </p>
         `
     }
